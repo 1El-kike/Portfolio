be8247737e109d79e0d823113b1c1db75f112935
Enrique <enriquegrassporras@gmail.com>
2024-05-02 14:38:44 -0400

    ...

--- src/components/Pagina_Principal/Main.jsx
+++ src/components/Pagina_Principal/Main.jsx
@@ -1,58 +1,57 @@
-import React, { useState } from 'react'
-import { PaginaPrincipal } from './PaginaPrincipal'
-import { AsideLeft } from './Aside/segundo_componente'
-import { Paginacion } from './Article_section/Paginacion'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/js/bootstrap'
-
+import React, { useState } from "react";
+import { PaginaPrincipal } from "./PaginaPrincipal";
+import { AsideLeft } from "./Aside/segundo_componente";
+import { Paginacion } from "./Article_section/Paginacion";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/js/bootstrap";
 
 export const Main = () => {
+  const [num, setnum] = useState(0);
+  const [page, setpage] = useState([]);
 
-    const [num, setnum] = useState(0)
-    const [page, setpage] = useState([])
+  const datospadre = (num) => {
+    setnum(num);
+    console.log(num);
+  };
 
+  const moverPaginas = (page) => {
+    setpage(page);
+  };
 
-    const datospadre = (num) => {
-        setnum(num)
-        console.log(num)
-    }
+  const [ancho, setancho] = useState(window.innerWidth);
 
-    const moverPaginas = (page) => {
-        setpage(page)
-    }
+  window.addEventListener("resize", function () {
+    var ancho = this.window.innerWidth;
+    setancho(ancho);
+  });
 
-
-    return (<>
-
-        <div className='container d-flex  gap-3 align-items-start'>
-
-
-            <div className='col-lg-4 col-md-4 justify-content-center  d-none d-md-block  sidebar'>
-                <AsideLeft className=" " />
-            </div>
-            <div style={{}} className='col-lg-8 col-md-8 col-12 order-2 mr-1 d-flex justify-content-center'>
-
-                <PaginaPrincipal
-                    moverPaginas={moverPaginas}
-                    num={num}
-                />
-
-            </div>
+  return (
+    <>
+      <div className="container d-flex  gap-3 align-items-start">
+        {ancho >= 992 && (
+          <div className="col-lg-4 col-md-4 justify-content-center  d-none d-md-block  sidebar">
+            <AsideLeft className=" " />
+          </div>
+        )}
+        <div
+          style={{}}
+          className="col-lg-8 col-md-12 col-12 order-2 mr-1 d-flex justify-content-center"
+        >
+          <PaginaPrincipal moverPaginas={moverPaginas} num={num} />
         </div>
+      </div>
 
-        <div className='w-100 col-12 d-flex justify-content-center align-items-center'>
-            <div className='col-12 m-3'>
-
-                <Paginacion
-                    datos={num}
-                    datospadre={datospadre}
-                    className="container bg-black"
-                    pagina={page}
-                />
-            </div>
+      <div className="w-100 col-12 d-flex justify-content-center align-items-center">
+        <div className="col-12 m-3">
+          <Paginacion
+            datos={num}
+            datospadre={datospadre}
+            className="container bg-black"
+            pagina={page}
+          />
         </div>
-
+      </div>
     </>
-    )
-}
+  );
+};
