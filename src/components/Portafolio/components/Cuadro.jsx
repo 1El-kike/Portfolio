8b11dcf1a3f667202e9af2842c5c9776bccbbd6d
Enrique Grass Porras <enriquegrassporras@gmail.com>
2026-03-03 11:44:18 -0500

    add more style

--- src/components/Portafolio/components/Cuadro.jsx
+++ src/components/Portafolio/components/Cuadro.jsx
@@ -6,17 +6,15 @@ export const Cuadro = ({ num, text }) => (
     className="border-2 border-yellow-400 rounded w-5/12 mt-2 mb-2"
   >
     <div className="flex">
-      <h2 className="font-semibold tracking-widest text-gray-700 text-yellow-500 text-6xl ps-2">
-        {num}
-      </h2>
-      <span className="top-5 mt-1 ms-2 font-semibold tracking-widest text-gray-700 text-yellow-500 text-2xl">
+      <p className="font-extrabold text-yellow-500 text-7xl ps-2">{num}</p>
+      <span className="top-5 mt-1 ms-2 font-extrabold tracking-widest text-gray-700 text-yellow-500 text-2xl">
         +
       </span>
     </div>
     <div className="flex overflow-hidden justify-between">
       <span className="h-px w-16 border-b-4 border-gray-600 rounded mx-auto" />
       <div className="">
-        <p className="-m-5 p-5 text-white">{text}</p>
+        <p className="p-1 mr-2  text-white">{text}</p>
       </div>
     </div>
   </div>
